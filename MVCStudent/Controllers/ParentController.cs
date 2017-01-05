b7a191035e70c39d9f81a5edad6d66df39fa5ef5
Grant Venter <Grant Venter@GRANT>
2017-01-05 17:50:14 +0200

    Add project files.

---
+++ MVCStudent/Controllers/ParentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using MVCStudent.Models;
+
+namespace MVCStudent.Controllers
+{
+    public class ParentsController : Controller
+    {
+        private MVCStudentEntities _Database = new MVCStudentEntities();
+
+        public ActionResult Parent(Parent parent)
+        {
+            return View("ParentDetails", parent);
+        }
+
+        //Save Button
+        [HttpPost]
+        public ActionResult Save(Parent model)
+        {
+            if (ModelState.IsValid)
+            {
+                _Database.Entry(model).State = EntityState.Modified;
+                _Database.SaveChanges();
+
+                return RedirectToAction("StudentDetails", "Students");
+            }
+
+            return View("Parent", model);
+        }
+    }
+}
---
+++ MVCStudent/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using MVCStudent.Models;
+
+namespace MVCStudent.Controllers
+{
+    public class StudentController :Controller
+    {
+        private MVCStudentEntities _Database = new MVCStudentEntities();
+        IQueryable<Student> students;
+
+        public ActionResult StudentInfo()
+        {
+            students = _Database.Students.Include(s => s.Course).Include(s => s.Parent);
+            return View(students.ToList());
+        }
+
+        public ActionResult Parent(string ID)
+        {
+            var parent = _Database.Parents.Find(ID);
+            return RedirectToAction("Parent", "Parents", parent);
+        }
+    }
+}
